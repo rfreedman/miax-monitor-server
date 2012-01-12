9bcf8d07e2aa16e91a29969c07bba65652c6ae4e
Rich Freedman <rfreedman@miami-holdings.com>
2012-01-12 16:18:05 -0500

    created one generic publisher class instead of having a distinct class per channel

--- grails-app/services/monitor/server/MongoStatsService.groovy
+++ grails-app/services/monitor/server/MongoStatsService.groovy
@@ -7,11 +7,10 @@ import com.mongodb.DB
 import com.mongodb.DBCollection
 import grails.converters.JSON
 import org.springframework.beans.factory.InitializingBean
-import org.cometd.bayeux.Channel
+
 import org.cometd.bayeux.server.BayeuxServer
 import org.cometd.bayeux.server.ServerSession
 import org.cometd.bayeux.server.ServerMessage.Mutable
-import java.util.concurrent.atomic.AtomicInteger
 
 class MongoStatsService implements InitializingBean {
 
@@ -23,21 +22,23 @@ class MongoStatsService implements InitializingBean {
     def bayeuxSession
 
     private HashMap<String, Runnable> publishers = new HashMap<String, Runnable>();
-    ///private HashMap<String, Set<String>> subscriptions = new HashMap<String, Set<String>>();
-
-
-
 
     void afterPropertiesSet() {
 
-        publishers.put("/rollups/mei-capacity-by-cloud",  new MeiCapacityPublisher())
-        publishers.put("/rollups/mei-latency-by-cloud",   new MeiLatencyPublisher())
+        // TODO: generate publishers based on configuration
+        // TODO: have configuration of supported stats (publishers), and a known list of roll-up permutations, and generate publishers from configuration
+        StatPublisher publisher = new StatPublisher("/rollups/mei-capacity-by-cloud", "MeiCapacityPublisher", meiCapacityRollup)
+        publishers.put(publisher.channelName, publisher)
+
+        publisher = new StatPublisher("/rollups/mei-latency-by-cloud", "MeiLatencyPublisher", meiLatencyRollup)
+        publishers.put(publisher.channelName, publisher)
 
         bayeux.addExtension(new MetaListenerExtension());
         bayeuxSession = bayeux.newLocalSession()
         bayeuxSession.handshake()
     }
 
+    // TODO: can we do away with the 'empty' rollup closures, or at least generate the emoties dynamically, in way similar to what getInitialValues() does ?
     def emptyMeiCapacityRollup = {
         def stats = [];
 
@@ -95,10 +96,10 @@ class MongoStatsService implements InitializingBean {
 
     private Object getInitialValues(String appType, String statType) {
         DBCollection collection = db.getCollection("current_stats");
-        def sampleStat = collection.findOne([type:appType, stat:statType]);
+        def sampleStat = collection.findOne([type: appType, stat: statType]);
         def stats = sampleStat.stats;
         def retval = [:]
-        for(stat in stats) {
+        for (stat in stats) {
             retval[stat.key] = 0
         }
         return retval
@@ -118,8 +119,6 @@ class MongoStatsService implements InitializingBean {
             def stats = [];
 
             for (Integer key: rollUps.keySet()) {
-                //System.out.println(key + ": " + rollUps.get(key));
-
                 def aStat = [key];
                 Map<String, Integer> rollup = rollUps.get(key)
                 for (Map.Entry<String, Integer> entry: rollup.entrySet()) {
@@ -156,7 +155,7 @@ class MongoStatsService implements InitializingBean {
             return emptyMeiLatencyRollup()
         }
     }
-    
+
     public static void main(String[] args) throws Exception {
 
         MongoStatsService service = new MongoStatsService()
@@ -191,29 +190,27 @@ class MongoStatsService implements InitializingBean {
         */
     }
 
-    class MeiCapacityPublisher implements Runnable {
-
+    class StatPublisher implements Runnable {
+        private String channelName;
+        private String description;
+        private Closure statClosure;
         private shouldRun = false;
-        private AtomicInteger subscriberCount = new AtomicInteger(0);
 
-        public def addSubscriber =  {
-            def currentCount = subscriberCount.incrementAndGet().intValue()
-            println("MeiCapacityPbublisher - new subscriber, count now ${currentCount}")
-
-            if(!publishing) {
-                println "MeiCapacityPublisher - got subscription, and not publishing yet, so starting to publish"
-                new Thread(this).start()
-            }
+        public StatPublisher(String channelName, String description, Closure statClosure) {
+            this.channelName = channelName
+            this.description = description
+            this.statClosure = statClosure
         }
 
-        public def removeSubscriber = {
-           def currentCount = subscriberCount.decrementAndGet().intValue()
-           println "MeiCapacityPublisher - subscriber removed, count now ${currentCount}"
-           if(currentCount < 1) {
-               subscriberCount.set(0)
-               shouldRun = false
-               println "MeiCapacityPublisher - last subscriber unsubscribed, so stopping"
-           }
+        public def getChannelName = {
+            channelName
+        }
+
+        public def ensurePublishing = {
+            if (!publishing) {
+                println "${description} - got subscription, and not publishing yet, so starting to publish"
+                new Thread(this).start()
+            }
         }
 
         public boolean isPublishing() {
@@ -224,18 +221,25 @@ class MongoStatsService implements InitializingBean {
             shouldRun = false;
         }
 
+        private def hasSubscribers = {
+          def channel = bayeux.getChannel(channelName)
+          channel.getSubscribers().size() > 0
+        }
+
+        private def getChannel = {
+            bayeuxSession.getChannel(channelName)
+        }
+
         void publishRollup() {
-
-            def rollUp = meiCapacityRollup()
-            //long before = new Date().getTime()
-
-            def jsonData = ["payload": ["rollup": rollUp]] as JSON
-            def jsonString = jsonData.toString()
-            bayeuxSession.getChannel('/rollups/mei-capacity-by-cloud').publish(jsonString)
-
-            //long after = new Date().getTime()
-            //long duration = after - before
-            //println "** published mei capacity rollup in ${duration} msec."
+            if (hasSubscribers()) {
+                def rollUp = statClosure()
+                def jsonData = ["payload": ["rollup": rollUp]] as JSON
+                def jsonString = jsonData.toString()
+                getChannel().publish(jsonString)
+            } else {
+                println "${description}: no subscribers, so stopping"
+                stopPublishing()
+            }
         }
 
         void run() {
@@ -246,14 +250,14 @@ class MongoStatsService implements InitializingBean {
             while (shouldRun) {
                 try {
                     def age = new Date().getTime() - lastPublished
-                    if (age >= 2000 - lastDuration) {
+                    if (age >= 2000 - lastDuration) { // throttle publishing speed
                         def before = new Date().getTime()
                         publishRollup();
                         lastPublished = new Date().getTime();
                         def after = new Date().getTime()
                         def duration = after - before
                         lastDuration = duration
-                        println "published mei capacity rollup in ${duration} msec."
+                        println "${description}: published in ${duration} msec."
                     } else {
                         Thread.sleep(100);
                     }
@@ -262,117 +266,33 @@ class MongoStatsService implements InitializingBean {
                 }
             }
         }
-    }
 
-
-     class MeiLatencyPublisher implements Runnable {
-        private shouldRun = false;
-
-        private AtomicInteger subscriberCount = new AtomicInteger(0);
-
-        public def addSubscriber =  {
-            def currentCount = subscriberCount.incrementAndGet()
-            println("MeiLatencyPublisher - new subscriber, count now ${currentCount}")
-
-            if(!publishing) {
-                println "MeiLatencyPublisher - got subscription, and not publishing yet, so starting to publish"
-                new Thread(this).start()
-            }
-        }
-
-        public def removeSubscriber = {
-          def currentCount = subscriberCount.decrementAndGet()
-          println("MeiLatencyPublisher - removed subscriber, count now ${currentCount}")
-           if(currentCount < 1) {
-               subscriberCount.set(0)
-               shouldRun = false
-               println "MeiLatencyPublisher -  last subscriber unsubscribed, so stopping"
-           }
-        }
-
-        public boolean isPublishing() {
-            return shouldRun
-        }
-         
-        public void stopPublishing() {
-            shouldRun = false;
-        }
-
-        void publishRollup() {
-            def rollUp = meiLatencyRollup()
-            def jsonData = ["payload": ["rollup": rollUp]] as JSON
-            def jsonString = jsonData.toString()
-            bayeuxSession.getChannel('/rollups/mei-latency-by-cloud').publish(jsonString)
-        }
-
-        void run() {
-            long lastPublished = 0;
-            long lastDuration = 0;
-
-            shouldRun = true;
-
-            while (shouldRun) {
-                try {
-                    def age = new Date().getTime() - lastPublished
-                    if (age >= 2000 - lastDuration) {
-                        def before = new Date().getTime()
-                        publishRollup();
-                        lastPublished = new Date().getTime();
-                        def after = new Date().getTime()
-                        def duration = after - before
-                        lastDuration = duration
-                        println "published mei latency rollup in ${duration} msec."
-                    } else {
-                        Thread.sleep(100);
-                    }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }
     }
 
     class MetaListenerExtension implements BayeuxServer.Extension {
 
         boolean rcvMeta(ServerSession serverSession, Mutable meta) {
+            if (meta.channel.toString().endsWith("/subscribe")) {
 
-            for(;;) {
-                if(meta.channel.toString().endsWith("/subscribe")) {
+                def publisher = publishers.get(meta.subscription)
 
-                   def publisher = publishers.get(meta.subscription)
-
-                   if(publisher) {
-                       publisher.addSubscriber()
-                   } else {
-                       println "didn't find publisher for subscribe to ${meta.subscription}"
-                   }
-
-                   break;
+                if (publisher) {
+                    publisher.ensurePublishing()
+                } else {
+                    println "didn't find publisher for subscribe to ${meta.subscription}: publishers = ${publishers}"
                 }
-
-                if(meta.channel.toString().endsWith("/unsubscribe")) {
-                   def publisher = publishers.get(meta.subscription)
-                   if(publisher) {
-                       publisher.removeSubscriber()
-                   } else {
-                       println "didn't find publisher for unsubscribe from ${meta.subscription}"
-                   }
-
-                    break;
-                }
-
-                break;
             }
-
             return true
         }
 
         boolean rcv(ServerSession serverSession, Mutable mutable) {
             return true
         }
+
         boolean send(ServerSession serverSession, ServerSession serverSession1, Mutable mutable) {
             return true
         }
+
         boolean sendMeta(ServerSession serverSession, Mutable mutable) {
             return true
         }
